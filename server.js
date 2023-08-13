b1d5c32e3f16830be006c257977b7d5eb630fe28
alecvipa <hebrit_626@hotmail.com>
2023-08-13 12:59:33 -0600

    finished site

--- server.js
+++ server.js
@@ -64,11 +64,11 @@ app.post("/ajax/email", function (request, response) {
     });
 
     
-    var htmlBody = `<h2>Correo de contacto</h2><p>Nombre: ${request.body.name} </p> <p>Número de contacto: ${request.body.number} </p> <p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p>`;
+    var htmlBody = `<h2>Correo de contacto</h2><p>Nombre: ${request.body.name} </p> <p>Número de contacto: ${request.body.number} </p> <p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Mensaje: ${request.body.message} </p>`;
     var mail = {
         from: '"Team: Xyncs Web Studio',
         to: 'hebrit_626@hotmail.com',
-        subject: '¡Alguien ha dejado sus datos en Terraza i662!',
+        subject: '¡Nuevo Mensaje en Los Taqueñitos!',
         html: htmlBody
     };
     transporter.sendMail(mail, function (err, info) {
